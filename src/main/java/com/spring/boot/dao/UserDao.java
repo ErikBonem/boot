9c6d5a077b4e0787e15b8428a7b68d92ffb06527
ErikBonem <yunusoverik@mail.ru>
2022-09-16 19:55:10 +0300

    123

--- src/main/java/com/spring/boot/dao/UserDao.java
+++ src/main/java/com/spring/boot/dao/UserDao.java
@@ -2,8 +2,6 @@ package com.spring.boot.dao;
 
 import com.spring.boot.models.User;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -17,15 +15,12 @@ public class UserDao {
     public User show(Long id){
         return em.find(User.class,id);
     }
-    @Transactional
     public void save(User user){
         em.persist(user);
     }
-    @Transactional
     public void update (User user){
         em.merge(user);
     }
-    @Transactional
     public void delete(Long id){
         em.remove(show(id));
     }
